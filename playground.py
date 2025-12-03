1fe956b4b941b697dd897d7fb1c276ac120f1008
sparisi <sparisi@users.noreply.github.com>
2025-12-03 10:35:02 +0100

    update playground with diagonal actions

--- playground.py
+++ playground.py
@@ -1,22 +1,27 @@
 """
-Move:   ← ↑ → ↓
-Stay:   ENTER
-Reset:  R
-Quit:   Q
+Move:   ↖ ↑ ↗      Q W E
+        ←   →  or  A   D
+        ↙ ↓ ↘      Z X C
+Stay:   ENTER or S
+Reset:  Backspace
+Quit:   Esc
 
 python playground.py ENVIRONMENT --record --env-arg= ...
 --record to save a gif of the game
+--discount to also compute discounted rewards
 --env-arg to pass optional environment arguments
 
 Example:
-python playground.py Gym-Gridworlds/TravelField-28x28-v0 --env-arg distance_reward=True --env-arg no_stay=True --record
+python playground.py Gym-Gridworlds/TravelField-28x28-v0 --env-arg distance_reward=True --env-arg no_stay=True --record --discount=0.99
 """
 
 import imageio
 import numpy as np
 import gymnasium
 import gym_gridworlds
-from gym_gridworlds.gridworld import LEFT, DOWN, RIGHT, UP, STAY
+from gym_gridworlds.gridworld import (
+    LEFT, DOWN, RIGHT, UP, STAY, DOWN_LEFT, DOWN_RIGHT, UP_LEFT, UP_RIGHT
+)
 import argparse
 from pynput import keyboard
 import time
@@ -25,7 +30,8 @@ import json
 
 # Mutable so we can update it in on_press`
 program_running = [True]
-sum_rewards = [0]
+sum_rewards = [0.0, 0.0]
+t = [0]
 
 def parse_env_args(arg_list):
     env_kwargs = {}
@@ -49,6 +55,7 @@ parser.add_argument(
     help="Extra environment arguments in key=value format",
 )
 parser.add_argument("--record", action="store_true")
+parser.add_argument("--discount", default=1.0)
 args = parser.parse_args()
 
 env_kwargs = parse_env_args(args.env_arg)
@@ -67,16 +74,21 @@ def step(action):
     state = env.unwrapped.agent_pos
     next_obs, rwd, term, trunc, info = env.step(action)
     sum_rewards[0] = sum_rewards[0] + rwd
+    sum_rewards[1] = sum_rewards[1] + rwd * args.discount**t[0]
     next_state = env.unwrapped.agent_pos
+    t[0] += 1
 
     print(f"{state} | {action} | {rwd} | {next_state}")
 
-    if term:
-        print("Terminal state, sum of rewards:", sum_rewards[0])
-        sum_rewards[0] = 0
-    elif trunc:
-        print("Time step limit, sum of rewards:", sum_rewards[0])
-        sum_rewards[0] = 0
+    if term or trunc:
+        if term:
+            print(f"Terminal state ...")
+        elif trunc:
+            print(f"Time step limit ...")
+        print(f"  ... sum of rewards: {sum_rewards[0]} (undiscounted), {sum_rewards[1]} (discounted)")
+        reset()
+        sum_rewards[0] = 0.0
+        sum_rewards[1] = 0.0
 
     if args.record:
         env_record.step(action)
@@ -86,10 +98,13 @@ def step(action):
 
 
 def reset():
+    print("\nEnvironment reset")
     seed = np.random.randint(999)
     env.reset(seed=seed)
     env.render()
-    sum_rewards[0] = 0
+    sum_rewards[0] = 0.0
+    sum_rewards[1] = 0.0
+    t[0] = 0
 
     if args.record:
         env_record.reset(seed=seed)
@@ -99,22 +114,32 @@ def reset():
 
 
 def on_press(key):
+    vk = getattr(key, "vk", None)
+    ch = getattr(key, "char", None)
     try:
-        if key == keyboard.Key.up or key == keyboard.Key.num8:
+        if key == keyboard.Key.up or vk == 104 or ch == "w":
             step(UP)
-        elif key == keyboard.Key.down or key == keyboard.Key.num2:
+        elif key == keyboard.Key.down or vk == 98 or ch == "x":
             step(DOWN)
-        elif key == keyboard.Key.left or key == keyboard.Key.num4:
+        elif key == keyboard.Key.left or vk == 100 or ch == "a":
             step(LEFT)
-        elif key == keyboard.Key.right or key == keyboard.Key.num6:
+        elif key == keyboard.Key.right or vk == 102 or ch == "d":
             step(RIGHT)
-        elif key == keyboard.Key.enter or key == keyboard.Key.num5:
+        elif key == keyboard.Key.enter or vk == 101 or ch == "s":
             step(STAY)
-        elif key.char.isalpha() and key.char == "q":
+        elif key == keyboard.Key.enter or vk == 105 or ch == "e":
+            step(UP_RIGHT)
+        elif key == keyboard.Key.enter or vk == 99 or ch == "c":
+            step(DOWN_RIGHT)
+        elif key == keyboard.Key.enter or vk == 103 or ch == "q":
+            step(UP_LEFT)
+        elif key == keyboard.Key.enter or vk == 97 or ch == "z":
+            step(DOWN_LEFT)
+        elif key == keyboard.Key.esc:
             # Can't call env.close() or pygame will freeze everything
             program_running[0] = False
             return False
-        elif key.char.isalpha() and key.char == "r":
+        elif key == keyboard.Key.backspace:
             reset()
         else:
             pass
@@ -124,12 +149,14 @@ def on_press(key):
 
 print(
     "\n"
-    "Move: \t← ↑ → ↓\n"
-    "Stay: \tENTER\n"
-    "Reset: \tR\n"
-    "Quit: \tQ\n"
+    "Move: \t↖ ↑ ↗      Q W E\n"
+    "\t←   →  or  A   D\n"
+    "\t↙ ↓ ↘      Z X C\n"
+    "Stay: \tENTER or S\n"
+    "Reset: \tBackspace\n"
+    "Quit: \tEsc\n"
     "\n"
-    "Prints are: (S | A | R | S')"
+    "Prints are: (State | Action | Reward | Next State)"
 )
 
 reset()
