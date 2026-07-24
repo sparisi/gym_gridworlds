c590a70e315520268c064e4bb8fafd5e026afb83
sparisi <sparisi@users.noreply.github.com>
2026-07-24 22:02:47 +0300

    pyproject

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from setuptools import setup
 
 setup(
     name="Gym-Gridworlds",
-    version="1.0",
+    version="1.0.0",
     license="CC-BY-4.0",
     author="Simone Parisi",
     packages=["gym_gridworlds"],
+    package_data={"gym_gridworlds": ["grids/*.txt"]},
+    include_package_data=True,
+    python_requires=">=3.9",
     install_requires=["gymnasium", "pygame", "numpy"],
     extras_require={
         "playground": ["pynput", "imageio"],
